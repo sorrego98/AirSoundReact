1b2240f3fa9c6c8aba2869b3766417985f813fa8
Guede <Florencia.Guede@chubb.com>
2023-10-26 00:07:28 -0300

    prueba ok

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ContentWrapper from "./ContentWrapper";
+function App() {
+  return (
+    <React.Fragment>
+        <ContentWrapper />
+    </React.Fragment>
+  );
+}
+
+export default App;
