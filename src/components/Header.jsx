91edfd0ed2a4d50cdd72400d8ab2f77a803c70d2
sorrego98 <juan.sorrego98@gmail.com>
2023-10-28 09:26:09 -0500

    Header Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Menu, MenuItem, MenuButton, SubMenu } from '@szhsin/react-menu';
 import '@szhsin/react-menu/dist/index.css';
 import '@szhsin/react-menu/dist/transitions/slide.css';
-import { RiNotification2Line, RiArrowDownCircleLine, RiSettings4Fill, RiLogoutBoxRLine } from "react-icons/ri";
+import { RiNotification2Line, RiArrowDownCircleLine, RiSettings4Fill, RiLogoutBoxRLine, RiHeart3Fill, RiChat4Line } from "react-icons/ri";
 
 const Header = () => {
   return (
@@ -16,10 +16,51 @@ const Header = () => {
               2
             </span>
           </MenuButton>
-        } transition menuClassName="bg-secondary-300" >
-          <h1 className='text-center font-medium'>Notifications</h1>
+        } transition align='end' menuClassName="bg-secondary-300" >
+          <h1 className='text-center font-medium'>
+            Notifications
+          </h1>
+          <hr className='my-4 border-gray-500' />
           <MenuItem className="p-0 hover:bg-transparent">
-            <Link to="/" className='rounded-lg transition-colors hover:bg-secondary-100 flex items-center gap-x-4 py-2 px-6 flex-1'>
+            <Link to="/" className='rounded-lg transition-colors hover:bg-secondary-100 flex items-center gap-2 py-2 px-4 flex-1'>
+              <img src="https://img.freepik.com/foto-gratis/colocar-joven-mujer-ropa-deportiva_144627-45298.jpg" alt="Ana Lievano" className='w-9 h-9 object-cover rounded-full' />
+              <div className='text-sm flex flex-col gap-2'>
+                <div className='flext items-center justify-between gap-4'>
+                  <span>Ana Lievano</span>{" "}
+                  <span className='text-xs'>28/10/2023</span>
+                </div>
+                <p className='text-gray-700 '>Lorem ipsum dolor sit amet...</p>
+              </div>
+            </Link>
+          </MenuItem>
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link to="/" className='rounded-lg transition-colors hover:bg-secondary-100 items-center gap-2 py-2 px-4 flex flex-1'>
+              <RiHeart3Fill className='p-2 bg-red-300 text-red-700 box-content rounded-full' />
+              <div className='text-sm flex flex-col gap-2'>
+                <div className='flext items-center justify-between gap-4'>
+                  <span>Nueva reacción</span>{" "}
+                  <span className='text-xs'>28/10/2023</span>
+                </div>
+                <p className='text-gray-700 '>A Marciano García le ha gustado...</p>
+              </div>
+            </Link>
+          </MenuItem>
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link to="/" className='rounded-lg transition-colors hover:bg-secondary-100 items-center gap-2 py-2 px-4 flex flex-1'>
+              <RiChat4Line className='p-2 bg-purple-300 text-purple-700 box-content rounded-full' />
+              <div className='text-sm flex flex-col gap-2'>
+                <div className='flext items-center justify-between gap-4'>
+                  <span>Nuevo comentario</span>{" "}
+                  <span className='text-xs'>28/10/2023</span>
+                </div>
+                <p className='text-gray-700 '>Ana Lievano ha comentado tu pub...</p>
+              </div>
+            </Link>
+          </MenuItem>
+          <hr className='my-4 border-gray-500' />
+          <MenuItem className="p-0 hover:bg-transparent flex justify-center cursor-default">
+            <Link to="/" className='text-gray-800 text-sm hover:text-black'>
+              Todas las notificaciones
             </Link>
           </MenuItem>
         </Menu>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { RiBarChartGroupedFill, RiEarthFill, RiMessage2Line, RiCalendar2Line, RiLogoutBoxRLine, RiArrowRightCircleLine} from "react-icons/ri";
-import { MdSupportAgent } from "react-icons/md";
+import { RiBarChartGroupedFill, RiEarthFill, RiMessage2Line, RiCalendar2Line, RiLogoutBoxRLine, RiArrowRightCircleLine, RiCustomerServiceFill} from "react-icons/ri";
 
 const Sidebar = () => {
     const [showSubmenu, setShowSubmenu] = useState(false);
@@ -52,7 +51,7 @@ const Sidebar = () => {
                             </li>
                             <li>
                                 <Link to="/" className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-200 transition-colors'>
-                                    <MdSupportAgent /> Technical Support
+                                    <RiCustomerServiceFill /> Technical Support
                                 </Link>
                             </li>
                             <li>
